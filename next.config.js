9cf102393181c16f947a241cf9a06447d1953edd
stelian.corbut <stelian.corbut@gmail.com>
2025-02-04 17:06:55 +0200

    Fix GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
 const nextConfig = {
   output: 'export', // Face site-ul static
   distDir: 'out', // Next.js va exporta HTML aici
+  assetPrefix: process.env.NODE_ENV === 'production' ? "/docs-app/" : "", // Necesită pentru GitHub Pages
+  images: { unoptimized: true }, // Evită optimizarea imaginilor (GitHub Pages nu suportă Image Optimization)
+  trailingSlash: true, // Adaugă `/` la sfârșitul URL-urilor pentru compatibilitate mai bună
 }
 
 module.exports = nextConfig;
