95ee76566582d4850445a31bd57391bb7ebb2b43
stelian.corbut <stelian.corbut@gmail.com>
2025-02-04 17:53:49 +0200

    corectie la next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@
 const nextConfig = {
   output: 'export', // Face site-ul static
   distDir: 'out', // Next.js va exporta HTML aici
-  assetPrefix: process.env.NODE_ENV === 'production' ? "/docs-app/" : "", // Necesită pentru GitHub Pages
+  assetPrefix: '/docs-app/', // Necesită pentru GitHub Pages
+  basePath: '/docs-app', 
   images: { unoptimized: true }, // Evită optimizarea imaginilor (GitHub Pages nu suportă Image Optimization)
   trailingSlash: true, // Adaugă `/` la sfârșitul URL-urilor pentru compatibilitate mai bună
 }
